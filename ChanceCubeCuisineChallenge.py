1f190fae2f75aae2e197f74e0eb5b4bb3e2b9ab7
Chris O'Rawe <chris.orawe@geh.nhs.uk>
2022-08-30 10:19:06 +0100

    Made it a bit pretty now.

--- ChanceCubeCuisineChallenge.py
+++ ChanceCubeCuisineChallenge.py
@@ -1,13 +1,33 @@
 import os
 import random
+from tabnanny import filename_only
 
 def main():
+
+    print("\n Atomic Shrimp: Chance Cube Cuisine Challenge\n ============================================\n")
+
+    dirFiles = []
+    maxLenOfFilename = 0
 
     for file in os.listdir('.'):
 
         filename, fileExtension = os.path.splitext(file)
 
         if fileExtension == '.txt':
+
+            dirFiles.append(file)
+
+            lenOfFilename = len(filename)
+
+            if lenOfFilename > maxLenOfFilename:
+
+                maxLenOfFilename = lenOfFilename
+
+    if len(dirFiles) > 0:
+
+        for file in dirFiles:
+
+            filename, fileExtension = os.path.splitext(file)
 
             with open(file) as f:
 
@@ -33,13 +53,19 @@ def main():
 
                 else:
 
-                    choiceList.append("Error: Required Choice Count ({0}) > Choices ({1})".format(requiredChoiceCount, len(fileContentList)))
+                    choiceList.append(" Error: Required Choice Count ({0}) > Choices ({1})".format(requiredChoiceCount, len(fileContentList)))
 
             else:
 
                 choiceList.append(random.choices(fileContentList)[0].strip('\n'))
 
-            print("{0}: {1}".format(filename, ', '.join(choiceList)))
+            print(" {0:}{1} : {2}".format(filename, (maxLenOfFilename - len(filename)) * ' ', ', '.join(choiceList)))
+
+    else:
+
+        print(" Error: No choice files available...")
+            
+    print()
 
 if __name__ == "__main__":
 
